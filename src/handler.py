9b08e435f40c74847efc7a52ac08d7853a84a9f0
olcortesb <oscar.cortes@ackstorm.com>
2024-02-23 22:50:47 +0100

    add body and post

--- src/handler.py
+++ src/handler.py
@@ -2,9 +2,8 @@ import json
 from src.client_bedrock import titan_model 
 
 def hello(event, context):
-
-    question = "how to calculate the factorial of a number"
-    text = titan_model(question)
+    q = json.loads(event['body'])
+    text = titan_model(json.dumps(q['question']))
     response = {"statusCode": 200, "body": json.dumps(text)}
 
     return response
